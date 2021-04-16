06984274e3638f3598ab90caad6df311653daa61
Stiliyan Stoyanov <76836234+StoyanovSt@users.noreply.github.com>
2021-04-16 15:34:59 +0300

    Added Hotel Component

---
+++ client/src/components/Hotel/Hotel.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+const Hotel = () => {
+    return (
+        <Link to="" className="added-hotel">
+            <img src="" alt=""
+                className="picture-added-hotel" />
+            <h3>Hilton Toronto</h3>
+            <span>Free rooms: </span>
+        </Link>
+    );
+}
+
+export default Hotel;
